7f8b0be9dd490b9b78f8e7b845f04248a364e631
Igor Korchagin <igor.korchagin@lemonone.com>
2020-08-23 23:42:57 +0300

    v2.0.7

--- src/getChangedWorkspacesIds.js
+++ src/getChangedWorkspacesIds.js
@@ -11,10 +11,12 @@ const getChangedWorkspacesIds = async ({
   const formatName = (str) => (keyNaming ? _[keyNaming](str) : str);
   const normalizeName = (str) => (namespace ? str.replace(namespace, "") : str);
   const workspaces = await getChangedWorkspaces({ branch, projectRoot });
-  return Object.entries(workspaces).reduce((obj, [id, files]) => {
-    if (files.length <= 0) return obj;
-    return { ...obj, [formatName(normalizeName(id))]: files };
-  }, {});
+  const result = {};
+  Object.entries(workspaces).map(([id, files]) => {
+    if (files.length <= 0) return;
+    result[formatName(normalizeName(id))] = files;
+  });
+  return result;
 };
 
 module.exports.getChangedWorkspacesIds = getChangedWorkspacesIds;
